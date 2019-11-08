9aeef00762dd6aac64f0b49a0952491861892c32
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-11-07 21:39:19 -0800

    Adding Functionality to Delete, Clear and Filter Tasks
    Adding Functionality to Delete, Clear and Filter Tasks

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const loadEventListeners = () => {
             alert('Add a Valid task!');
             return;
         }
-        
+
         //Create an li element to put inside the tasklist above
         const li = document.createElement('li');
         //Add class
@@ -34,6 +34,49 @@ const loadEventListeners = () => {
         //Clear input
         taskInput.value = '';
     });
+
+    //Add the delete task event - Since the delete links are being generated dynamically, you need to have a delegation of events and not have to select the individual links. This means that we have to attach the event listener to the parent component
+    taskList.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target.parentElement.classList.contains('delete-item')) {
+            if (confirm('Are you sure you want to delete this task?')) {
+                event.target.parentElement.parentElement.remove();
+            }
+        }
+    });
+
+    //Clear All Tasks Event
+    clearBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const tasks = document.querySelectorAll('li');
+        if (confirm('Are you sure you want to clear all your tasks?')) {
+            tasks.forEach(taskItem => {
+                taskItem.remove();
+            });
+        }
+    });
+
+    //Filter through the tasks event
+    filter.addEventListener('input', event => {
+        event.preventDefault();
+        if (!event.target.value) {
+            document.querySelectorAll('li').forEach(listItem => {
+                listItem.style.display = 'block';
+            });
+            return;
+        }
+
+        const text = event.target.value.toLowerCase();
+        document.querySelectorAll('li').forEach(listItem => {
+            const itemText = listItem.firstChild.textContent;
+            if (itemText.toLowerCase().includes(text)) {
+                listItem.style.display = 'block';
+            } else {
+                listItem.style.display = 'none';
+            }
+        });
+    });
+
 };
 
 //Local Storage
